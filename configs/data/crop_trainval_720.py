0a3131685eb95de507d623a1755bfc9323449c20
Steve <Steve@Steves-MacBook-Pro.local>
2023-06-30 06:29:46 +0800

    adapt crop

--- configs/data/crop_trainval_720.py
+++ configs/data/crop_trainval_720.py
@@ -17,6 +17,6 @@ cfg.DATASET.MIN_OVERLAP_SCORE_TEST = 0.0   # for both test and val
 
 # 368 scenes in total for MegaDepth
 # (with difficulty balanced (further split each scene to 3 sub-scenes))
-cfg.TRAINER.N_SAMPLES_PER_SUBSET = 100
+cfg.TRAINER.N_SAMPLES_PER_SUBSET = 1000
 
 cfg.DATASET.MGDPT_IMG_RESIZE = 720  # for training on 32GB meme GPUs
