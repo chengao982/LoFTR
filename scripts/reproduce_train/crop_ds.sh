c37122beef2ffa32e1eadbfa2775a2955171d72d
Steve <Steve@student-net-vpn-0259.intern.ethz.ch>
2023-06-30 16:03:04 +0800

    adapt crop

--- scripts/reproduce_train/crop_ds.sh
+++ scripts/reproduce_train/crop_ds.sh
@@ -32,4 +32,5 @@ python -u ./train.py \
     --limit_val_batches=1. \
     --num_sanity_val_steps=10 \
     --benchmark=True \
-    --max_epochs=30
+    --max_epochs=30 \
+    --ckpt_path="weights/outdoor_ds.ckpt"
