d8d291fd26baa1b35a0c42af3825a1baf0d94131
Steve <Steve@student-net-vpn-0259.intern.ethz.ch>
2023-06-30 17:04:43 +0800

    adapt crop

--- configs/data/crop_trainval_640.py
+++ configs/data/crop_trainval_640.py
@@ -20,5 +20,5 @@ cfg.DATASET.MIN_OVERLAP_SCORE_TEST = 0.0   # for both test and val
 cfg.TRAINER.N_SAMPLES_PER_SUBSET = 1000
 
 cfg.DATASET.MGDPT_IMG_RESIZE = 640  # for training on 32GB meme GPUs
-# cfg.DATASET.MGDPT_IMG_PAD = False  # pad img to square with size = MGDPT_IMG_RESIZE
-# cfg.DATASET.MGDPT_DEPTH_PAD = False  # pad depthmap to square with size = 2000
+cfg.DATASET.MGDPT_IMG_PAD = False  # pad img to square with size = MGDPT_IMG_RESIZE
+cfg.DATASET.MGDPT_DEPTH_PAD = False  # pad depthmap to square with size = 2000
