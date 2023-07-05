87e5b5beb0eeeb1bd28d1365ce441e6bba971a98
Steve <Steve@student-net-vpn-1007.intern.ethz.ch>
2023-07-05 23:53:35 +0800

    print test

--- scripts/reproduce_train/crop_ds.sh
+++ scripts/reproduce_train/crop_ds.sh
@@ -15,7 +15,7 @@ main_cfg_path="configs/loftr/outdoor/loftr_ds_dense.py"
 
 n_nodes=1
 n_gpus_per_node=2
-torch_num_workers=2
+torch_num_workers=8
 batch_size=1
 pin_memory=true
 exp_name="crop-ds-${TRAIN_IMG_SIZE}-bs=$(($n_gpus_per_node * $n_nodes * $batch_size))"
